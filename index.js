e405a0921341f956f39861e2ad072b637b4f12a2
Adewole Adebayo <wole1105@gmail.com>
2022-06-09 01:53:49 -0700

    commit tree 5d33c55f0eac871e1202d1adc9f4b4e702c730e0

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const input = document.querySelector('#input');
+
+function addingEventListener(){
+    input.addEventListener('click', () =>
+    alert('I was click'))
 }
+
+
+
